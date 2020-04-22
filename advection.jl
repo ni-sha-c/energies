f267cff491326fb3da8dea3f1b32fa6fb66b37d1
Nisha Chandramoorthy <nishac92@gmail.com>
2020-04-22 15:21:08 -0400

    Chebyshev method for advection equation added.

--- advection.jl
+++ advection.jl
@@ -2,6 +2,7 @@
 # \partial v/\partial t + a \partial v/\partial x = 0
 using PyPlot
 using LinearAlgebra
+include("cheb.jl")
 nsteps = 100
 tau = 0.02
 a = 2.0/tau
@@ -15,10 +16,20 @@ c = dt*a/dx
 A .= lmul!(c,A)
 A .= I(m) .- A  
 v1 = copy(v0)
-for n = 1:nsteps
-	plot(x,v1)
+Nc = 20
+D = cheb_diff_matrix(Nc)
+dtc = 0.1/Nc/a
+D .= I(Nc+1) - a.*dtc.*D
+xc = cheb_pts(Nc)
+p0 = sin.(pi*xc)
+p1 = copy(p0)
+fig, ax = subplots(1,2)
+for n = 1:1000
+	ax[1].plot(x,v1)
+	ax[2].plot(xc,p1)
 	pause(0.1)
 	mul!(v1,A,v1)
+	p1 .= D*p1
 end
 
 
