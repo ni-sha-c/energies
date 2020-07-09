620fc6a295269f95dc7c89a9a03b97dff6b0a93e
Nisha Chandramoorthy <nishac@mit.edu>
2020-07-09 09:44:49 -0400

    Least squares must have one more equation in the case of adjoint nnilss.

---
+++ src/adjoint_lsssolve.jl
@@ -0,0 +1,22 @@
+# LSS solve
+using SparseArrays
+using BlockArrays
+function lsssolve(R, b, vf, Qf)
+    d_u, n = size(b)
+    ndu = n*d_u
+    eye = sparse(Matrix([zeros(ndu, d_u) 1.0I(ndu)]))
+    D = (BlockArray{Float64}(zeros(ndu,ndu),
+               d_u*ones(Int64,n),
+               d_u*ones(Int64,n)))
+    [D[Block(i,i)] = R[:,:,i] for i =1:n]
+    D = sparse([Array(D) zeros(ndu, d_u)])
+    B = D - eye
+	pf_Q = [Qf zeros(d_u,1)][:]
+	B = sparse([Array(B); pf_Q'])
+    BB = B*transpose(B)
+	b1 = [b[:]; -vf]
+
+	a = -transpose(B)*(BB\b1[:])
+    a = reshape(a, d_u, n+1)[:,1:end-1]
+	return a
+end
