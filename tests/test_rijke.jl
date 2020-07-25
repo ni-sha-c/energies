dd6d8a27df989283b90f65db2c04b328c89898d1
Nisha Chandramoorthy <nishac@mit.edu>
2020-07-25 16:43:09 -0400

    remove objective function, not required

--- tests/test_rijke.jl
+++ tests/test_rijke.jl
@@ -1,20 +1,11 @@
 include("../examples/rijke.jl")
 include("../src/lss.jl")
-function Jac(u,s,n)
-	u1 = Rijke_ODE(u,s,n)[:,end]
-	return sum(x->x*x, u1[1:2*Ng])
-end
-function Jray(u,s,n)
-	u1 = Rijke_ODE(u,s,n)[:,end]
-	return dot(cjpixf, u1[1:Ng])
-end
 function Rijke_tangent_sensitivity(n_spe)
 	s = [7.0, 0.2]
 	n = 2000
 	d = N
 
 	du_trj = zeros(d, d, n)
-	J_trj = ones(2,n)
 	dJ_trj = zeros(2,d,n)
 	f_trj = zeros(d,n)
 	X_trj = zeros(d,n)
@@ -26,8 +17,6 @@ function Rijke_tangent_sensitivity(n_spe)
 
 	for i = 2:n
 		vel_p_i = view(u, 1:2*Ng)
-		J_trj[1,i] = 0.25*sum(x->x*x,vel_p_i)
-		J_trj[2,i] = dot(cjpixf, vel_p_i[1:Ng]) 
 		dJ_trj[1,1:2*Ng,i] = 0.5*vel_p_i
 		dJ_trj[2,1:Ng,i] = cjpixf
 		X_trj[:,i] = perturbation(u, s, 1.e-6)
@@ -35,14 +24,16 @@ function Rijke_tangent_sensitivity(n_spe)
 		du_trj[:,:, i] = dRijke(u, s, 1.e-6)
 		u = Rijke_ODE(u, s, 1)
 	end
-	y, xi = lss(du_trj, X_trj, zeros(d,n), s, 3)
+	y, xi = lss(du_trj, X_trj, f_trj, s, 2)
 	dJds = compute_sens(y, xi, dJ, f_trj)
 	vsh[:,:] = y
 	save(string("../data/rijke_tangent_sensitivity/",
 		    "vsh_and_dJds_", string(n_spe), 
 		    ".jld"), "vsh", vsh, "dJds", dJds)
 end
-
+function collect_sensitivity()
+	pmap(Rijke_tangent_sensitivity, 1:240)
+end
 
 #=
 	n_samples = 1000
