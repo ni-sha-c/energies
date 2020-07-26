4c5f58a3c83e2463913d6d0a50243290a4ea04c1
Nisha Chandramoorthy <nishac@mit.edu>
2020-07-26 12:30:10 -0400

    plotting tangent sensitivities.

--- utils/plot_rijke_sensitivities.jl
+++ utils/plot_rijke_sensitivities.jl
@@ -24,6 +24,14 @@ function plot_tangent_sensitivities()
 	indices = -10.0 .<= dJds_arr[1,:] .<= 10.0
 	n_files = sum(indices)
 	dJds_arr = dJds_arr[:,indices]
+	X = load("../data/sensitivities/sensitivity_t2000_dt01_n500.jld")
+	dJds = X["dJdbeta"]
+	indices = -10.0 .<= dJds[1,:] .<= 10.0
+	n_files += sum(indices)
+	dJds = dJds[:,indices]
+	dJds_arr = [dJds_arr dJds]
+
+
 	dJds_avg = sum(dJds_arr, dims=2)/n_files
 	dJds_var = sum((dJds_arr .- dJds_avg).^2.0)/n_files 
 	fig, ax = subplots(1,1)
@@ -34,8 +42,8 @@ function plot_tangent_sensitivities()
 	ax.yaxis.set_tick_params(labelsize=25)
 	ax1.yaxis.set_tick_params(labelsize=25)
 	
-	ax.plot(1:n_files, dJds_arr[1,:], "b.", label=L"$\dfrac{d \langle E_{\rm ac}\rangle}{d \beta}$",ms=2.0)
-	ax1.plot(1:n_files, dJds_arr[2,:], "r.", label=L"$\dfrac{d \langle E_{\rm ray}\rangle}{d \beta}$",ms=2.0)
+	ax.plot(1:n_files, dJds_arr[1,:], "b.", label=L"$\dfrac{d \langle J_{\rm ac}\rangle}{d \beta}$",ms=2.0)
+	ax1.plot(1:n_files, dJds_arr[2,:], "r.", label=L"$\dfrac{d \langle J_{\rm ray}\rangle}{d \beta}$",ms=2.0)
 	ax.plot(1:n_files, ones(n_files)*dJds_avg[1],"b",lw=2.0)
 	ax1.plot(1:n_files, ones(n_files)*dJds_avg[2],"r",lw=2.0)
 	ax.set_xlabel("Sample #",fontsize=25)
@@ -47,6 +55,72 @@ function plot_tangent_sensitivities()
 	fig.legend(fontsize=25)
 	return vsh_arr, dJds_arr, dJds_avg, dJds_var
 end
+function plot_tangent_sens_cum_avg()
+	n_files = 480
+	dJds_avg = zeros(2)
+	dJds_var = zeros(2)
+	dJds_arr = zeros(2,n_files)
+	d = 30 
+	nSteps = 2000
+	vsh_arr = zeros(d, nSteps, n_files)
+	for n = 1:n_files 
+		filename = string("../data/",
+						  "rijke_tangent_sensitivities/",
+						  "vsh_and_dJds_",
+						  string(n),
+						  ".jld")
+
+		X = load(filename)
+		dJds = X["dJds"]
+		vsh = X["vsh"]
+		dJds_arr[:,n] = dJds
+		vsh_arr[:,:,n] = vsh
+	end
+	indices = -10.0 .<= dJds_arr[1,:] .<= 10.0
+	n_files = sum(indices)
+	dJds_arr = dJds_arr[:,indices]
+	X = load("../data/sensitivities/sensitivity_t2000_dt01_n500.jld")
+	dJds = X["dJdbeta"]
+	indices = -10.0 .<= dJds[1,:] .<= 10.0
+	n_files += sum(indices)
+	dJds = dJds[:,indices]
+	dJds_arr = [dJds_arr dJds]
+
+
+	dJds_avg = sum(dJds_arr, dims=2)/n_files
+	dJds_var = sum((dJds_arr .- dJds_avg).^2.0)/n_files 
+	fig, ax = subplots(1,1)
+	fig1, ax1 = subplots(1,1)
+	
+	ax.xaxis.set_tick_params(labelsize=25)
+	ax1.xaxis.set_tick_params(labelsize=25)
+	ax.yaxis.set_tick_params(labelsize=25)
+	ax1.yaxis.set_tick_params(labelsize=25)
+	T = 20.0 
+	ax.plot(T*(1:n_files), 
+			cumsum(dJds_arr[1,:])./(1:n_files),  
+			"r.",  label=L"$\dfrac{d \langle J_{\rm ac}\rangle}{d \beta}$",ms=4.0)
+	ax.set_ylim([-3.25,-2.4])
+	ax1.plot(T*(1:n_files), 
+			 cumsum(dJds_arr[2,:])./(1:n_files), 
+			 "b.", label=L"$\dfrac{d \langle J_{\rm ray}\rangle}{d \beta}$",ms=4.0)
+	ax.plot(T*(1:n_files), ones(n_files)*dJds_avg[1],"r",lw=2.0)
+	ax1.plot(T*(1:n_files), ones(n_files)*dJds_avg[2],"b",lw=2.0)
+	ax.set_xlabel("Time",fontsize=25)
+	ax.set_ylabel("Tangent shadowing sensitivities",fontsize=25)
+	ax.grid(true)
+	ax1.set_xlabel("Time",fontsize=25)
+	ax1.set_ylabel("Tangent shadowing sensitivities",fontsize=25)
+	ax1.set_ylim([-0.03,0.03])
+	ax1.grid(true)
+	lgnd = fig.legend(loc=(0.5,0.7),fontsize=25)
+	lgnd.legendHandles[1]._legmarker.set_markersize(10)
+	lgnd = fig1.legend(loc=(0.5,0.2),fontsize=25)
+	lgnd.legendHandles[1]._legmarker.set_markersize(10)
+
+	return vsh_arr, dJds_arr, dJds_avg, dJds_var
+end
+
 function plot_adjoint_sensitivities()
 	n_files = 480
 	dJds_avg = zeros(2)
